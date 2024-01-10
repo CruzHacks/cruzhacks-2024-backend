adceeac8a13d0000c4c2a4f7d3d95925ab30a812
Zack Traczyk <me@zacktraczyk.com>
2024-01-09 20:46:44 -0800

    indexes; no tshrit/dietary conversion

--- functions/src/controllers/statistics.ts
+++ functions/src/controllers/statistics.ts
@@ -93,7 +93,7 @@ const cleanupDemographics = (demographics: Dict<string>) => {
     const age = Number(demographics.age);
 
     if (age < 18) {
-      demographics.age = "0-17";
+      demographics.age = "< 18";
     } else if (age >= 18 && age <= 25) {
       demographics.age = "18-25";
       demographics.age_range_18_to_25 = String(age);
@@ -170,6 +170,44 @@ const cleanupDemographics = (demographics: Dict<string>) => {
     demographics.sexual_orientation = sexualOrientation;
   }
 
+  if (demographics.ethnic_background) {
+    const providedEthnicBackground = [
+      "Asian Indian",
+      "Black/African",
+      "Chinese",
+      "Filipino",
+      "Korean",
+      "Guamanian/Chamorro",
+      "Japanese",
+      "Hispanic/Lation/Spanish Origin",
+      "Middle Eastern",
+      "Vietnamese",
+      "Native Hawaiian",
+      "Samoan",
+      "Native American or Alaskan Native",
+      "White",
+      "Other Pacific Islander",
+      "Other Asian (Cambodian, Thai, etc.)",
+      "No Answer",
+    ];
+
+    let ethnicBackground = demographics.ethnic_background;
+
+    if (ethnicBackground.toLowerCase().includes("no")) {
+      ethnicBackground = "No Answer";
+    }
+
+    if (!providedEthnicBackground.includes(ethnicBackground)) {
+      ethnicBackground = "Other";
+    }
+
+    demographics.ethnic_background = ethnicBackground;
+  }
+
+  if (demographics.graduation_year === "2028 and beyond") {
+    demographics.graduation_year = "2028+";
+  }
+
   if ((demographics.ucsc_college_affiliation as string).includes("N/A")) {
     demographics.ucsc_college_affiliation = "N/A";
   }
@@ -223,6 +261,50 @@ const cleanupLogistics = (logistics: Dict<string>) => {
     (logistics.attendence_possible_wo_reimbursement as string).includes("No")
   ) {
     logistics.attendence_possible_wo_reimbursement = "No";
+  }
+
+  if (logistics.tshirt_size) {
+    const providedTshirtSizes = [
+      "XS",
+      "S",
+      "M",
+      "L",
+      "XL",
+      "XXL",
+      "XXXL",
+      "No Answer",
+    ];
+
+    let tshirtSize = logistics.tshirt_size;
+
+    if (!providedTshirtSizes.includes(tshirtSize)) {
+      tshirtSize = "Other";
+    }
+
+    logistics.tshirt_size = tshirtSize;
+  }
+
+  if (logistics.dietary_restrictions) {
+    const providedDietaryRestrictions = [
+      "Gluten-Free",
+      "Vegan",
+      "Vegetarian",
+      "Peanut Allergies",
+      "Lactose Intolerant",
+      "None",
+    ];
+
+    let dietaryRestrictions = logistics.dietary_restrictions;
+
+    if (dietaryRestrictions.toLowerCase().includes("n/a")) {
+      dietaryRestrictions = "None";
+    }
+
+    if (!providedDietaryRestrictions.includes(dietaryRestrictions)) {
+      dietaryRestrictions = "Other";
+    }
+
+    logistics.dietary_restrictions = dietaryRestrictions;
   }
 
   cleanupNoAnswer(logistics);
@@ -279,6 +361,7 @@ app.post("/generate", async (req, res) => {
         year_in_school: {},
         graduation_year: {},
         area_of_study: {},
+        area_of_study_cs_ce_gd_other: {},
         hackathon_experience: {},
         first_cruzhacks: {},
       },
@@ -332,6 +415,18 @@ app.post("/generate", async (req, res) => {
 
       if (data.age && data.age >= 18 && data.age <= 25) {
         data.age_range_18_to_25 = String(data.age);
+      }
+
+      if (data.area_of_study) {
+        if (data.area_of_study === "Computer and Information Science") {
+          data.area_of_study_cs_ce_gd_other = "CS";
+        } else if (data.area_of_study === "Computer Engineering") {
+          data.area_of_study_cs_ce_gd_other = "CE";
+        } else if (data.area_of_study === "Game Design") {
+          data.area_of_study_cs_ce_gd_other = data.area_of_study;
+        } else {
+          data.area_of_study_cs_ce_gd_other = "Other";
+        }
       }
 
       data.ucsc_vs_non_ucsc =
@@ -403,7 +498,19 @@ app.post("/generate", async (req, res) => {
         approvalRate: statistics.submissions.approvalRate,
       },
       demographics: dictToRechartsArray(statistics.demographics),
-      logistics: dictToRechartsArray(statistics.logistics),
+      logistics: {
+        ...dictToRechartsArray({
+          need_travel_reimbursement:
+            statistics.logistics.need_travel_reimbursement,
+          need_charter_bus: statistics.logistics.need_charter_bus,
+          need_campus_parking_permit:
+            statistics.logistics.need_campus_parking_permit,
+          attendence_possible_wo_reimbursement:
+            statistics.logistics.attendence_possible_wo_reimbursement,
+        }),
+        tshirt_size: statistics.logistics.tshirt_size,
+        dietary_restrictions: statistics.logistics.dietary_restrictions,
+      },
       referral: dictToRechartsArray(statistics.referral),
     };
 
@@ -417,7 +524,7 @@ app.post("/generate", async (req, res) => {
 
     logger.info("Statistics recharts pre-computed; Written to firestore");
 
-    res.status(200).send({ data: rechartsStatistics } as APIResponse);
+    res.status(201).send({ data: rechartsStatistics } as APIResponse);
   } catch (err) {
     const error = ensureError(err);
     res.status(500).send({ error: error.message } as APIResponse);
@@ -429,8 +536,18 @@ app.post("/generate", async (req, res) => {
  */
 app.get("/", async (req, res) => {
   try {
-    console.log("monkey");
-    res.status(200).send("Hello World!");
+    const statistics = await getFirestore()
+      .collection("statistics")
+      .doc("pre_computed_recharts")
+      .get();
+
+    if (!statistics.exists) {
+      throw new Error("Statistics do not exist");
+    }
+
+    const data = statistics.data();
+
+    res.status(200).send({ data } as APIResponse);
   } catch (err) {
     const error = ensureError(err);
     logger.error(error);
